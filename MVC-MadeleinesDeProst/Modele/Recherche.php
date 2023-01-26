ef48de22ec61054e7d274823992fcedaa1aefb6b
Arthur Lecomte <92148174+Arthur-Lecomte@users.noreply.github.com>
2023-01-26 15:21:04 +0100

    Add files via upload

--- MVC-MadeleinesDeProst/Modele/Recherche.php
+++ MVC-MadeleinesDeProst/Modele/Recherche.php
@@ -1,28 +1,28 @@
 <?php
 class Recherche
 {
-
     public static function rechercheRecette(){
         require 'BaseDeDonnes.php';
-                    $pdo = BaseDeDonnes::connexion();
-                    $listeNomRecette = $pdo->query("SELECT NOM FROM RECETTE");
-                    $texteRecherche = $_POST['recherche'];
-                    $resultat = [];
-                    foreach($listeNomRecette as $row) {
-                        $nomRecettePossible = strtolower($row["NOM"]);
-                        $pourcentage = 0;
-                        similar_text($texteRecherche,$nomRecettePossible,$pourcentage);
-                        if ($pourcentage == 100){
-                            $resultat = [$texteRecherche];
-                            break;
-                        }elseif($pourcentage>65){
-                            array_unshift($resultat,$nomRecettePossible);
-                        }elseif($pourcentage>35){
-                            array_push($resultat,$nomRecettePossible);
-                        }
-                    }
-                    foreach($resultat as $row){
-                        echo "<p class='recettePossible'> ".$row." </p><br>";
-                    }   
+        $pdo = BaseDeDonnes::connexion();
+        $listeNomRecette = $pdo->query("SELECT NOM FROM RECETTE");
+        $texteRecherche = $_POST['recherche'];
+        $resultat = [];
+        foreach($listeNomRecette as $row) {
+            $nomRecettePossible = strtolower($row["NOM"]);
+            $pourcentage = 0;
+            similar_text($texteRecherche,$nomRecettePossible,$pourcentage);
+            if ($pourcentage == 100){
+                $resultat = [$texteRecherche];
+                break;
+            }elseif($pourcentage>65){
+                array_unshift($resultat,$nomRecettePossible);
+            }elseif($pourcentage>35){
+                array_push($resultat,$nomRecettePossible);
+            }
+        }
+        foreach($resultat as $row){
+            echo "<p class='recettePossible'> ".$row." </p><br>";
+        }   
     }
-}
+}          
+?> 
