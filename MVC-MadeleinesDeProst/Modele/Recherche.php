254a16b7dac26fbad5f24aacfd70a491e8b93154
Arthur Lecomte <92148174+Arthur-Lecomte@users.noreply.github.com>
2023-01-26 18:32:11 +0100

    Add files via upload

--- MVC-MadeleinesDeProst/Modele/Recherche.php
+++ MVC-MadeleinesDeProst/Modele/Recherche.php
@@ -18,9 +18,6 @@ class Recherche
                 array_push($resultat,$nomRecettePossible);
             }
         }
-        foreach($resultat as $row){
-            $row[0] = strtoupper($row[0]);
-            echo "<p class='recettePossible'> ".$row." </p><br>";
-        }   
+        return $resultat;
     }
 }
