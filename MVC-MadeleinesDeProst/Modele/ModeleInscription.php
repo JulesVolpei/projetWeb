a517e755e2ae04b1114783097af75d2133dca466
Phyz <nathan.vigon@etu.univ-amu.fr>
2023-01-26 15:25:03 +0100

    insertion de l'utilisateur et verification de son nom

--- MVC-MadeleinesDeProst/Modele/ModeleInscription.php
+++ MVC-MadeleinesDeProst/Modele/ModeleInscription.php
@@ -4,7 +4,24 @@ class ModeleInscription
 
     public static function inscription($nom, $email, $mdp, $cheminFichier)
     {
-        $dbLink = BaseDeDonnes::connexion();
+        $dateInscription = date("Y-m-d");
+
+        $query = mysqli_prepare(BaseDeDonnes::connexion(), "INSERT INTO UTILISATEUR(MOT_DE_PASSE, IMAGE, NOM, DATE_PREM_CO, DATE_DER_CO, EMAIL) VALUES (?, ?, ?, ?, ?, ?);");
+        mysqli_stmt_bind_param($query, "ssssss", $mdp, $cheminFichier, $nom, $dateInscription, $dateInscription, $email);
+        mysqli_execute($query);
+        mysqli_stmt_close($query);
+    }
+
+    public static function verifyName($nom){
+
+        $queryNom = mysqli_prepare(BaseDeDonnes::connexion(), "Select NOM FROM UTILISATEUR Where NOM = ?");
+        mysqli_stmt_bind_param($queryNom, "s", $nom);
+        mysqli_execute($queryNom);
+        mysqli_stmt_bind_result($queryNom, $nom);
+        mysqli_stmt_fetch($queryNom);
+        mysqli_stmt_close($queryNom);
+
+        return $nom;
     }
 
 }
