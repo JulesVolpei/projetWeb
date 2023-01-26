d93645566399a37a0b48acb83411eae45fcc31b7
Jeremy <jeremy.gross@etu.univ-amu.fr>
2023-01-26 23:34:54 +0100

    style page recettes

--- MVC-MadeleinesDeProst/Vues/pageRecettes/afficherLesRecettes.php
+++ MVC-MadeleinesDeProst/Vues/pageRecettes/afficherLesRecettes.php
@@ -1,8 +1,10 @@
 <?php
+echo '<section>';
 foreach ($A_vue["recettes"] as $recette) {
-    // Echo les divs pour les recettes
-    echo '<div style="width: 200px; height: 200px; background-color: red;">
-          <img src="'. $recette["IMAGE"] .'" style="width: 100%;">
-          <p style="text-align: center;">' . $recette["NOM"] . '</p>
+  // Echo les divs pour les recettes
+  echo '<div>
+          <img src="' . $recette["IMAGE"] . '">
+          <p>' . $recette["NOM"] . '</p>
         </div>';
 }
+echo '</section>';
