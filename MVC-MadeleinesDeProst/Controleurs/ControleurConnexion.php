cb6517232e4f00a812c180dc4dea70b2436310ce
Phyz <nathan.vigon@etu.univ-amu.fr>
2023-01-27 10:33:22 +0100

    connexion fonctionnel

--- MVC-MadeleinesDeProst/Controleurs/ControleurConnexion.php
+++ MVC-MadeleinesDeProst/Controleurs/ControleurConnexion.php
@@ -8,18 +8,23 @@ final class ControleurConnexion
         if (isset($_POST["connexion"])){
             session_start();
 
-            $_SESSION['nom'] = $_POST["nom"];
+            $nom = $_POST["nom"];
             $_SESSION['mdp'] = $_POST["mdp"];
-            //echo "<script> alert(\"" . $_SESSION['nom']. false . "\");</script>";
             $_SESSION['connecte'] = false;
 
-            if (!ModeleConnexion::verifieNom($_SESSION['nom'])) {
+            if (!ModeleConnexion::verifieNom($nom)) {
                 echo "<script> alert(\"Le nom entré n'est pas valide\"); </script>";
             } else {
+                $_SESSION['nom'] = $nom;
                 $mdp = ModeleConnexion::getMotDePasse();
                 if (password_verify($_SESSION["mdp"], $mdp)) {
                     echo "<script> alert(\"Vous êtes connecté!\"); </script>";
                     $_SESSION["connecte"] = true;
+                    if (ModeleConnexion::getAdmin() == 1) {
+                        $_SESSION["Admin"] = true;
+                    } else {
+                        $_SESSION["Admin"] = false;
+                    }
                 } else {
                     echo "<script> alert(\"Mauvais mot de passe\"); </script>";
                 }
--- MVC-MadeleinesDeProst/Modele/ModeleConnexion.php
+++ MVC-MadeleinesDeProst/Modele/ModeleConnexion.php
@@ -2,20 +2,21 @@
 class ModeleConnexion
 {
     public static function verifieNom($nom) {
-        $query = " SELECT * FROM `UTILISATEUR` WHERE NOM = '".$nom."'";
-        $user = BaseDeDonnes::connexion() -> query($query);
+        $queryNom = "SELECT * FROM `UTILISATEUR` WHERE NOM = '".$nom."'";
+        $user = BaseDeDonnes::connexion() -> query($queryNom);
         if ($user -> num_rows > 0){
             return true;
         }
         return false;
     }
     public static function getMotDePasse(){
-        $queryMDP = mysqli_prepare(BaseDeDonnes::connexion(), "SELECT MOT_DE_PASSE FROM `UTILISATEUR` WHERE NOM = ?;");
-        mysqli_stmt_bind_param($queryMDP, "s", $_SESSION["nom"]);
-        mysqli_execute($queryMDP);
-        mysqli_stmt_bind_result($queryMDP, $mdp);
-        mysqli_stmt_fetch($queryMDP);
-        mysqli_stmt_close($queryMDP);
-        return $mdp;
+        $queryMDP = "SELECT MOT_DE_PASSE FROM `UTILISATEUR` WHERE NOM = '".$_SESSION['nom']."'";
+        $mdp = BaseDeDonnes::connexion() -> query($queryMDP) ->fetch_assoc();    
+        return current($mdp);
+    }
+    public static function getAdmin(){
+        $queryAdmin = "SELECT ADMIN FROM `UTILISATEUR` WHERE NOM = '".$_SESSION['nom']."'";
+        $admin = BaseDeDonnes::connexion() -> query($queryAdmin) ->fetch_assoc();    
+        return current($admin);
     }
 }
